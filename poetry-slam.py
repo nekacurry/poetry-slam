0fa51340cba1aa78f2e2fad44631336127e40f3f
nekacurry <anneka498@gmail.com>
2020-10-01 19:46:39 -0500

    added lines printed backwards function

--- poetry-slam.py
+++ poetry-slam.py
@@ -1,9 +1,18 @@
 bonedog = "bonedog.txt"
 
 def get_file_lines(filename):
-    infile = open(filename, "r")
-    for line in infile:
-        print(line)
+    infile = open(filename)
+    return infile.readlines()
+    infile.close()
 
 
-get_file_lines(bonedog)
+def lines_printed_backwards(lines_list):
+    infile = open(lines_list)
+    revLines = infile.readlines()
+    count = 72
+    for line in reversed(revLines):
+        count -= 1
+        print(str(count) + '\t' + line)
+
+lines_printed_backwards(bonedog)
+
